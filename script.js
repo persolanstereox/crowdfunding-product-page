c9c5ee22b52ba1cafc615fad95b708576d85ef7b
Piotr Jemielita <91134784+persolanstereox@users.noreply.github.com>
2022-10-24 14:09:54 +0200

    final script

--- script.js
+++ script.js
@@ -1,26 +1,28 @@
 const aboutNavigation = document.querySelector('.nav-about');
 const sectionAbout = document.querySelector('.donating');
+const burgerBtn = document.querySelector('.burger');
+const burgerLines = document.querySelectorAll('.line');
+const navMenu = document.querySelector('ul');
+const clickToScrollEl = document.querySelector('.test-scroll');
 const openModalBtns = document.querySelectorAll('.open-modal');
 const bookmarkBtn = document.querySelector('.btn-bookmark');
 const spanBookmark = document.querySelector('.span-bookmark');
 const money = document.querySelector('.backed-money');
 const backers = document.querySelector('.backers');
 const progressBar = document.querySelector('.progress');
-const bambooHowManyLeft = document.querySelector('.Bamboo-left');
-const blackEditionHowMuchLeft = document.querySelector('.Black__edition--left');
 const closeModalBtn = document.querySelector('.close-modal');
 const modalLabelchoosePledge = document.querySelectorAll('.to-choose');
-const modalHowManyLeft = document.querySelectorAll('.how-much-left');
+const radioBtnschoosePldge = document.querySelectorAll('.btn-choose-reward');
+const formButton = document.querySelectorAll('.form-button');
 const boxesEnterYourPledge = document.querySelectorAll('.choice-reward ');
 const modalTypeRewardBox = document.querySelectorAll('.modal-type-reward');
 const selectRewardBtns = document.querySelectorAll('.confirm-pledge');
-const pledgeInputs = document.querySelectorAll('.input-pledge');
 const gotItBtn = document.querySelector('.display-changes');
 const overlay = document.querySelector('.overlay');
 const modal = document.querySelector('.modal');
 const modalCompleted = document.querySelector('.modal-completed');
+let bookmarkStorage = localStorage.getItem('bookmark') || 'bookmark';
 
-let selectedReward;
 const ProjectStats = {
   money: 89914,
   backers: 5007,
@@ -39,11 +41,6 @@ const BlackEditionStand = {
 const backOptions = [BamboStand, BlackEditionStand];
 
 // Functions
-
-aboutNavigation.addEventListener('click', function (e) {
-  e.preventDefault();
-  sectionAbout.scrollIntoView({ behavior: 'smooth' });
-});
 
 const openModal = function () {
   modal.classList.remove('hidden');
@@ -78,6 +75,10 @@ const selectReward = function (rewardNum) {
     .getElementById(`reward-executable--${rewardNum}`)
     .classList.add('selected-reward');
   displayEnterPledgeBox(rewardNum);
+  radioBtnschoosePldge.forEach(btn => {
+    btn.classList.add('hidden');
+  });
+  radioBtnschoosePldge[rewardNum].classList.remove('hidden');
 };
 
 const updateStats = function (i) {
@@ -91,7 +92,7 @@ const formatCurrency = function (value, locale, currency) {
     style: 'currency',
     currency: currency,
     maximumFractionDigits: 0,
-    }).format(value);
+  }).format(value);
 };
 const displayUpdate = function (Stats) {
   console.log(Stats.money / 1000);
@@ -115,12 +116,30 @@ const displayUpdate = function (Stats) {
 
 //Event Listneners
 
+clickToScrollEl.addEventListener('click', function (e) {
+  e.preventDefault();
+  sectionAbout.scrollIntoView({ behavior: 'smooth' });
+});
+
+burgerBtn.addEventListener('click', function () {
+  navMenu.classList.toggle('active');
+  burgerLines.forEach(line => {
+    line.classList.toggle('active');
+  });
+});
+
 openModalBtns.forEach((btn, i) => {
   btn.addEventListener('click', function () {
     openModal();
-    
+
     selectReward(i);
     this.blur();
+    if (navMenu.classList.contains('active')) {
+      navMenu.classList.remove('active');
+      burgerLines.forEach(line => {
+        line.classList.remove('active');
+      });
+    }
   });
 });
 
@@ -131,17 +150,35 @@ document.addEventListener('keydown', function (e) {
   }
 });
 bookmarkBtn.addEventListener('click', function () {
-  this.classList.toggle('bookmarked');
-  spanBookmark.classList.toggle('bookmarkedToggle');
-  spanBookmark.classList.contains('bookmarkedToggle')
-    ? (spanBookmark.textContent = 'Bookmarked')
-    : (spanBookmark.textContent = 'Bookmark');
+  if (bookmarkStorage === 'bookmark') {
+    spanBookmark.textContent = 'Bookmarked';
+    bookmarkStorage = 'bookmarked';
+    this.classList.add('bookmarked');
+  } else {
+    spanBookmark.textContent = 'Bookmark';
+    bookmarkStorage = 'bookmark';
+    this.classList.remove('bookmarked');
+  }
+  localStorage.setItem('bookmark', bookmarkStorage);
 });
+if (bookmarkStorage === 'bookmarked') {
+  bookmarkBtn.classList.add('bookmarked');
+  spanBookmark.textContent = 'Bookmarked';
+}
+
 modalLabelchoosePledge.forEach((label, i) => {
-  label.addEventListener('click', function () {
+  label.addEventListener('click', function (e) {
+    e.preventDefault();
     selectReward(i);
   });
 });
+formButton.forEach((form, i) => {
+  form.addEventListener('click', e => {
+    e.preventDefault();
+    selectReward(i);
+  });
+});
+
 selectRewardBtns.forEach((btn, i) => {
   btn.addEventListener('click', function () {
     const minPledge = Number(
